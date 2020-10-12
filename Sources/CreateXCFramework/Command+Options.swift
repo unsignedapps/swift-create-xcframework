655cbb357851b738972502802e48db7308c04b4b
Eugene Bokhan <eugenebokhan@protonmail.com>
2020-10-12 13:10:55 +0300

    fix warnings

--- Sources/CreateXCFramework/Command+Options.swift
+++ Sources/CreateXCFramework/Command+Options.swift
@@ -13,23 +13,23 @@ extension Command {
         
         // MARK: - Package Loading
         
-        @Option(default: ".", help: ArgumentHelp("The location of the Package", valueName: "directory"))
-        var packagePath: String
+        @Option(help: ArgumentHelp("The location of the Package", valueName: "directory"))
+        var packagePath = "."
         
         
         // MARK: - Building
         
-        @Option(default: ".build", help: ArgumentHelp("The location of the build/cache directory to use", valueName: "directory"))
-        var buildPath: String
+        @Option(help: ArgumentHelp("The location of the build/cache directory to use", valueName: "directory"))
+        var buildPath = ".build"
         
-        @Option(default: .release, help: ArgumentHelp("Build with a specific configuration", valueName: "debug|release"))
-        var configuration: PackageModel.BuildConfiguration
+        @Option(help: ArgumentHelp("Build with a specific configuration", valueName: "debug|release"))
+        var configuration = PackageModel.BuildConfiguration.release
         
-        @Flag(default: true, inversion: .prefixedNo, help: "Whether to clean before we build")
-        var clean: Bool
+        @Flag(inversion: .prefixedNo, help: "Whether to clean before we build")
+        var clean = true
         
         @Flag(help: "Prints the available products and targets")
-        var listProducts: Bool
+        var listProducts = false
 
         @Option(help: "The path to a .xcconfig file that can be used to override Xcode build settings. Relative to the package path.")
         var xcconfig: String?
@@ -40,17 +40,17 @@ extension Command {
         @Option(help: ArgumentHelp("A list of platforms you want to build for. Can be specified multiple times. Default is to build for all platforms supported in your Package.swift, or all Apple platforms if omitted", valueName: TargetPlatform.allCases.map({ $0.rawValue }).joined(separator: "|")))
         var platform: [TargetPlatform]
         
-        @Option(default: ".", help: ArgumentHelp("Where to place the compiled .xcframework(s)", valueName: "directory"))
-        var output: String
+        @Option(help: ArgumentHelp("Where to place the compiled .xcframework(s)", valueName: "directory"))
+        var output = "."
         
         @Flag(help: "Whether to wrap the .xcframework(s) up in a versioned zip file ready for deployment")
-        var zip: Bool
+        var zip = false
         
         @Option(help: ArgumentHelp("The version number to append to the name of the zip file\n\nIf the target you are packaging is a dependency, swift-create-xcframework will look into the package graph and locate the version number the dependency resolved to. As there is no standard way to specify the version inside your Swift Package, --zip-version lets you specify it manually.", valueName: "version"))
         var zipVersion: String?
 
         @Flag(help: .hidden)
-        var githubAction: Bool
+        var githubAction: Bool = false
  
         
         // MARK: - Targets
