f5a528a4ad773c0b6d19f51b086a6151728378c0
funzin <nakazawa.fumito@gmail.com>
2021-10-11 19:39:12 +0900

    Fix indent

--- Package.swift
+++ Package.swift
@@ -5,13 +5,17 @@ import PackageDescription
 
 let dependencies: [Package.Dependency]
 #if swift(>=5.5)
-dependencies = [.package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.4.4")),
-                .package(name: "SwiftPM", url: "https://github.com/apple/swift-package-manager.git", .branch("release/5.5")),
-                .package(url: "https://github.com/apple/swift-tools-support-core.git", .exact("0.2.3"))]
+dependencies = [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.4.4")),
+    .package(name: "SwiftPM", url: "https://github.com/apple/swift-package-manager.git", .branch("release/5.5")),
+    .package(url: "https://github.com/apple/swift-tools-support-core.git", .exact("0.2.3"))
+]
 #else
-dependencies = [.package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.3.2")),
-                .package(name: "SwiftPM", url: "https://github.com/apple/swift-package-manager.git", .revision("swift-5.3.3-RELEASE")),
-                .package(url: "https://github.com/apple/swift-tools-support-core.git", .exact("0.1.12"))]
+dependencies = [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.3.2")),
+    .package(name: "SwiftPM", url: "https://github.com/apple/swift-package-manager.git", .revision("swift-5.3.3-RELEASE")),
+    .package(url: "https://github.com/apple/swift-tools-support-core.git", .exact("0.1.12"))
+]
 #endif
 
 let package = Package(
--- Sources/CreateXCFramework/PackageInfo.swift
+++ Sources/CreateXCFramework/PackageInfo.swift
@@ -70,32 +70,34 @@ struct PackageInfo {
 
         self.toolchain = try UserToolchain(destination: try .hostDestination())
 
-        let resources: UserManifestResources
         #if swift(>=5.5)
-            resources = try UserManifestResources(swiftCompiler: self.toolchain.swiftCompiler, swiftCompilerFlags: [])
+        let resources = try UserManifestResources(swiftCompiler: self.toolchain.swiftCompiler, swiftCompilerFlags: [])
         #else
-            resources = try UserManifestResources(swiftCompiler: self.toolchain.swiftCompiler)
+        let resources = try UserManifestResources(swiftCompiler: self.toolchain.swiftCompiler)
         #endif
         let loader = ManifestLoader(manifestResources: resources)
         self.workspace = Workspace.create(forRootPackage: root, manifestLoader: loader)
         
         #if swift(>=5.5)
-            self.graph = try self.workspace.loadPackageGraph(rootPath: root, diagnostics: self.diagnostics)
-            let swiftCompiler = toolchain.swiftCompiler
-            self.manifest = try tsc_await { ManifestLoader.loadRootManifest(at: root,
-                                                                            swiftCompiler: swiftCompiler,
-                                                                            swiftCompilerFlags: [],
-                                                                            identityResolver: DefaultIdentityResolver(),
-                                                                            on: DispatchQueue.global(qos: .background),
-                                                                            completion: $0)
-            }
-        #else
-            self.graph = self.workspace.loadPackageGraph(root: root, diagnostics: self.diagnostics)
-            self.manifest = try ManifestLoader.loadManifest (
-                packagePath: root,
-                swiftCompiler: self.toolchain.swiftCompiler,
-                packageKind: .root
+        self.graph = try self.workspace.loadPackageGraph(rootPath: root, diagnostics: self.diagnostics)
+        let swiftCompiler = toolchain.swiftCompiler
+        self.manifest = try tsc_await {
+            ManifestLoader.loadRootManifest(
+                at: root,
+                swiftCompiler: swiftCompiler,
+                swiftCompilerFlags: [],
+                identityResolver: DefaultIdentityResolver(),
+                on: DispatchQueue.global(qos: .background),
+                completion: $0
             )
+        }
+        #else
+        self.graph = self.workspace.loadPackageGraph(root: root, diagnostics: self.diagnostics)
+        self.manifest = try ManifestLoader.loadManifest (
+            packagePath: root,
+            swiftCompiler: self.toolchain.swiftCompiler,
+            packageKind: .root
+        )
         #endif
     }
 
